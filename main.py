dcbb7210528951536a8b4a3ec8e31f1dc2c288c4
Mash__ke <111083110+Mash-ke@users.noreply.github.com>
2022-10-22 21:10:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy")
+    return jsonify("Hey there, si ata leo uslide dm......qwertyygshxjkvdjsbxckgzcbchj xZGSX")
 
 
 @APP.route('/auth', methods=['POST'])
